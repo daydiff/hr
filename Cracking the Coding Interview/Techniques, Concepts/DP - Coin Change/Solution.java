99df77ee5cb72eceee523782576cdec3bec4f73c
Alexandr Tabakov <alex.daymon@gmail.com>
2018-02-20 21:29:45 +0500

    DP - coin change

---
+++ Cracking the Coding Interview/Techniques, Concepts/DP - Coin Change/Solution.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static long makeChange(int[] coins, int money) {
+        long[] changes = new long[money + 1];
+        changes[0] = 1;
+        
+        for (int coin : coins) {
+            for (int i = coin; i <= money; i++) {
+                changes[i] += changes[i - coin];
+            }
+        }
+        
+        return changes[money];
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int coins[] = new int[m];
+        for(int coins_i=0; coins_i < m; coins_i++){
+            coins[coins_i] = in.nextInt();
+        }
+        System.out.println(makeChange(coins, n));
+    }
+}
